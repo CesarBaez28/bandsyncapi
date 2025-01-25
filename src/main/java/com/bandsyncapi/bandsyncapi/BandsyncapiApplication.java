044d46c109e70db6f9f134c4b0dcb390181b788a
César Báez <cesarbaez@Cesars-Laptop.local>
2025-01-24 22:40:16 -0400

    Initial commit for the bansync api

---
+++ src/main/java/com/bandsyncapi/bandsyncapi/BandsyncapiApplication.java
@@ -0,0 +1,13 @@
+package com.bandsyncapi.bandsyncapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandsyncapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandsyncapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bandsyncapi/bandsyncapi/BandsyncapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandsyncapi.bandsyncapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandsyncapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
