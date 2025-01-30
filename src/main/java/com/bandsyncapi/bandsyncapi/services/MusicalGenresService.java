f42ae64e52377fdf232bd044c74e5039562b59d7
César Báez <cesarbaez@Cesars-Laptop.local>
2025-01-29 23:04:00 -0400

    Adding documentation for methods in MusicalGenresService

--- src/main/java/com/bandsyncapi/bandsyncapi/services/MusicalGenresService.java
+++ src/main/java/com/bandsyncapi/bandsyncapi/services/MusicalGenresService.java
@@ -11,11 +11,29 @@ import com.bandsyncapi.bandsyncapi.models.MusicalGenresModel;
  */
 public interface MusicalGenresService {
 
+  /**
+   * Finds all musical genres in the database.
+   * @return A list with all musical genres in the database.
+   */
   public List<MusicalGenresModel> findAll();
 
+  /**
+   * Finds a musical genre by its id.
+   * @param id - Id of the musical genre to be found.
+   * @return An Optional with the musical genre if found, or an empty Optional if not found.
+   */
   public Optional<MusicalGenresModel> findById(Integer id);
 
+  /**
+   * Saves a musical genre to the database.
+   * @param musicalGenresModel - Musical genre to be saved.
+   * @return The saved musical genre.
+   */
   public MusicalGenresModel save(MusicalGenresModel musicalGenresModel);
 
+  /**
+   * Deletes a musical genre from the database by its id.
+   * @param id - Id of the musical genre to be deleted.
+   */
   public void deleteById(Integer id);
 }
