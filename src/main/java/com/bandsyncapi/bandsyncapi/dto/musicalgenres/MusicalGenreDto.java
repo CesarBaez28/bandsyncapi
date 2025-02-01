afb95dcb30f3a37c44c6360dd76ddd581c4fbf59
César Báez <cesarbaez@Cesars-Laptop.local>
2025-02-01 16:38:04 -0400

    Creating DTOs and mapper for the MusicalGenreModel

---
+++ src/main/java/com/bandsyncapi/bandsyncapi/dto/musicalgenres/MusicalGenreDto.java
@@ -0,0 +1,8 @@
+package com.bandsyncapi.bandsyncapi.dto.musicalgenres;
+
+/**
+ * This record is a DTO for the MusicalGenreModel.
+ */
+public record MusicalGenreDto(Integer id, String name, Boolean status) {
+  
+}
