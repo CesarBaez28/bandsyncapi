2a11914d54a23249c1fadc138e5227b42ce9545e
César Báez <cesarbaez@Cesars-Laptop.local>
2025-02-03 22:56:40 -0400

    Adding validations to DTOs

--- src/main/java/com/bandsyncapi/bandsyncapi/api/v1/dto/musicalbands/MusicalBandsDto.java
+++ src/main/java/com/bandsyncapi/bandsyncapi/api/v1/dto/musicalbands/MusicalBandsDto.java
@@ -2,9 +2,27 @@ package com.bandsyncapi.bandsyncapi.api.v1.dto.musicalbands;
 
 import java.util.UUID;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 /**
- * This record represents the data transfer object for the musical_bands table in the database.
+ * This record represents the data transfer object for the MusicalBandsModel
  */
-public record MusicalBandsDto(UUID id, String name, String logo, String address, String phone, String email, Boolean status) {
+public record MusicalBandsDto(
+  UUID id, 
+
+  @NotBlank(message = "El nombre es obligatorio.")
+  @Size(min = 3, max = 100, message = "El nombre debe tener entre 3 y 100 caracteres.")
+  String name, 
+
+  String logo, 
+  String address, 
+  String phone, 
+
+  @NotBlank(message = "El email es obligatorio.")
+  @Email(message = "El email debe ser válido.")
+  String email, 
   
-}
+  Boolean status
+) {}
