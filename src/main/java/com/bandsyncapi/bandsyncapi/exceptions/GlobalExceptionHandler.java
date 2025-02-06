56302cfa7c925acc1ab30487879715bdbac9a447
CesarBaez28 <baezcesar329@gmail.com>
2025-02-05 22:31:34 -0400

    Modifying GlobalExceptionHandler to catch DataIntegrityViolationException

--- src/main/java/com/bandsyncapi/bandsyncapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/bandsyncapi/bandsyncapi/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.bandsyncapi.bandsyncapi.exceptions;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -20,18 +21,17 @@ public class GlobalExceptionHandler {
   /**
    * Handle general exceptions
    * 
-   * @param ex - Exception
    * @return - An object ApiResponse with the error
    */
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<ApiResponse<Void>> handleGeneralException(Exception ex) {
+  public ResponseEntity<ApiResponse<Void>> handleGeneralException() {
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(new ApiResponse<>(false, "An unexpected error occurs", null, null));
   }
 
   /**
    * Handle field validations errors
-   * @param ex - Exception
+   * 
    * @return - An object ApiResponse with the field errors
    */
   @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -47,5 +47,39 @@ public class GlobalExceptionHandler {
         errors);
 
     return ResponseEntity.badRequest().body(response);
+  }
+
+  /**
+   * Handle integrity violation exception
+   * 
+   * @return - An object ApiResponse with the error
+   */
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  public ResponseEntity<ApiResponse<Void>> handleIntegrityViolationException(DataIntegrityViolationException ex) {
+    String errorMesage = ex.getMostSpecificCause().getMessage();
+    final Map<String, String> CONSTRAINT_MESSAGES = new HashMap<>();
+
+    CONSTRAINT_MESSAGES.put("musical_bands.name", "Ya existe una banda con ese nombre.");
+    CONSTRAINT_MESSAGES.put("musical_genres.musical_band_id", "El género ya está registrado para esta banda.");
+
+    String key = extractConstraintName(errorMesage);
+
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(new ApiResponse<>(false,
+            CONSTRAINT_MESSAGES.getOrDefault(key, "Ya existe un registro con ese nombre"), null, null));
+  }
+
+  /**
+   * Extract the constrain name from the database
+   * @param errorMessage - The error message from the exception
+   * @return - A string with the constrain name
+   */
+  private String extractConstraintName(String errorMessage) {
+    if (errorMessage.contains("Duplicate entry")) {
+      int startIndex = errorMessage.indexOf("for key '") + 9;
+      int endIndex = errorMessage.indexOf("'", startIndex);
+      return errorMessage.substring(startIndex, endIndex);
+    }
+    return "unknown_constraint";
   }
 }
