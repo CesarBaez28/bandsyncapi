6feb35003864335445c6d3886111268651c60a2c
CesarBaez28 <baezcesar329@gmail.com>
2025-02-15 21:49:08 -0400

    Fixing error in documentation

--- src/main/java/com/bandsyncapi/bandsyncapi/api/v1/dto/musicalgenres/MusicalGenrePutDto.java
+++ src/main/java/com/bandsyncapi/bandsyncapi/api/v1/dto/musicalgenres/MusicalGenrePutDto.java
@@ -1,7 +1,7 @@
 package com.bandsyncapi.bandsyncapi.api.v1.dto.musicalgenres;
 
 /**
- * This record represent a Dto to update the musical genre name
+ * This record represents a Dto to update the musical genre name
  */
 public record MusicalGenrePutDto(String name) {
   
