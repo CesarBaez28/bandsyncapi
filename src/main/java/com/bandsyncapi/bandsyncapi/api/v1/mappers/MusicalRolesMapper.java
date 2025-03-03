84219b67abdce8abdfed689ec464c378f19718ce
CesarBaez28 <baezcesar329@gmail.com>
2025-03-02 22:40:46 -0400

    Deleting import

--- src/main/java/com/bandsyncapi/bandsyncapi/api/v1/mappers/MusicalRolesMapper.java
+++ src/main/java/com/bandsyncapi/bandsyncapi/api/v1/mappers/MusicalRolesMapper.java
@@ -2,7 +2,6 @@ package com.bandsyncapi.bandsyncapi.api.v1.mappers;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
