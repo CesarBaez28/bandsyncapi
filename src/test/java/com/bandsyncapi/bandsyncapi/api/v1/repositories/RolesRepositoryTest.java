9a9eb1158b18523887a62dc4e6fcd4f7cfa94033
CesarBaez28 <baezcesar329@gmail.com>
2025-04-16 15:42:06 -0400

    Testing RolesRepository

---
+++ src/test/java/com/bandsyncapi/bandsyncapi/api/v1/repositories/RolesRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.bandsyncapi.bandsyncapi.api.v1.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.bandsyncapi.bandsyncapi.api.v1.models.MusicalBandsModel;
+import com.bandsyncapi.bandsyncapi.api.v1.models.RolesModel;
+
+@DataJpaTest
+class RolesRepositoryTest {
+  
+  @Autowired
+  private RolesRepository rolesRepository;
+
+  @Autowired
+  private MusicalBandsRepository musicalBandsRepository;
+
+  @Test
+  void testUpdateRoleNameById() {
+    // Given
+
+    var musicalBand = MusicalBandsModel.builder()
+        .name("Test Band")
+        .logo("test_logo.png")
+        .address("Test Address")
+        .phone("123456789")
+        .email("testEmail@gmail.com")
+        .status(true)
+        .build();
+    var savedMusicalBand = musicalBandsRepository.save(musicalBand);
+
+    var role = RolesModel.builder()
+    .name("Role Test")
+    .musicalBand(savedMusicalBand)
+    .status(true)
+    .build();
+
+    var savedRole = rolesRepository.save(role);
+
+    // When 
+    int result = rolesRepository.updateRoleNameById("Updated Role name", savedRole.getId());
+
+    // Then
+    assertEquals(1, result);
+  }
+}
