fcbbfb03933116a770ffcdc3f3db39e574101b8a
CesarBaez28 <baezcesar329@gmail.com>
2025-04-24 10:29:17 -0400

    Add unit tests for UsersMusicalBandsServiceImpl to verify save functionality

---
+++ src/test/java/com/bandsyncapi/bandsyncapi/api/v1/services/impl/UsersMusicalBandsServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.bandsyncapi.bandsyncapi.api.v1.services.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.bandsyncapi.bandsyncapi.api.v1.models.MusicalBandsModel;
+import com.bandsyncapi.bandsyncapi.api.v1.models.UsersModel;
+import com.bandsyncapi.bandsyncapi.api.v1.models.UsersMusicalBandsModel;
+import com.bandsyncapi.bandsyncapi.api.v1.repositories.UsersMusicalBandsRepository;
+
+@ExtendWith(MockitoExtension.class)
+class UsersMusicalBandsServiceImplTest {
+
+  @Mock
+  private UsersMusicalBandsRepository usersMusicalBandsRepository;
+
+  @InjectMocks
+  private UsersMusicalBandsServiceImpl usersMusicalBandsServiceImpl;
+
+  @Test
+  void testSave () {
+    // Given
+    var user = new UsersModel(UUID.randomUUID());
+    var musicalBand = new MusicalBandsModel(UUID.randomUUID());    
+
+    // When
+    usersMusicalBandsServiceImpl.save(user, musicalBand);
+
+    // Then
+    ArgumentCaptor<UsersMusicalBandsModel> captor = ArgumentCaptor.forClass(UsersMusicalBandsModel.class); 
+
+    verify(usersMusicalBandsRepository).save(captor.capture());
+    var usersMusicalBandsModel = captor.getValue();
+    
+    assertEquals(user.getId(), usersMusicalBandsModel.getUser().getId());
+    assertEquals(musicalBand.getId(), usersMusicalBandsModel.getMusicalBand().getId());
+  }
+}
