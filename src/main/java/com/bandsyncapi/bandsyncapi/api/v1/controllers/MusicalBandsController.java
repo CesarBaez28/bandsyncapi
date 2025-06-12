6b0a4630907a87ba762853d4896b3438fc98c3c6
CesarBaez28 <baezcesar329@gmail.com>
2025-06-12 17:19:32 -0400

    Remove unused FilesService import from MusicalBandsController

--- src/main/java/com/bandsyncapi/bandsyncapi/api/v1/controllers/MusicalBandsController.java
+++ src/main/java/com/bandsyncapi/bandsyncapi/api/v1/controllers/MusicalBandsController.java
@@ -8,7 +8,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.bandsyncapi.bandsyncapi.api.v1.dto.musicalbands.MusicalBandsDto;
 import com.bandsyncapi.bandsyncapi.api.v1.dto.musicalbands.MusicalBandsPostDto;
 import com.bandsyncapi.bandsyncapi.api.v1.models.UsersModel;
-import com.bandsyncapi.bandsyncapi.api.v1.services.FilesService;
 import com.bandsyncapi.bandsyncapi.api.v1.services.MusicalBandsService;
 import com.bandsyncapi.bandsyncapi.api.v1.services.UsersMusicalBandsService;
 import com.bandsyncapi.bandsyncapi.response.ApiResponse;
