d539900f1a1d2cc9b7a7955ef24f4f7469a77153
CesarBaez28 <baezcesar329@gmail.com>
2025-07-18 15:30:07 -0400

    Remove unused import for User from UsersController

--- src/main/java/com/bandsyncapi/bandsyncapi/api/v1/controllers/UsersController.java
+++ src/main/java/com/bandsyncapi/bandsyncapi/api/v1/controllers/UsersController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
