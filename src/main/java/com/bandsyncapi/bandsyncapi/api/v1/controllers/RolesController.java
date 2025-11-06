c29ca15f54fa69fef5b56c66db4c3a5e7156f352
CesarBaez28 <baezcesar329@gmail.com>
2025-11-06 15:24:27 -0400

    chore: Update Spring Boot version to 3.5.7 and Java version to 25 in pom.xml; remove unused import in RolesController

--- src/main/java/com/bandsyncapi/bandsyncapi/api/v1/controllers/RolesController.java
+++ src/main/java/com/bandsyncapi/bandsyncapi/api/v1/controllers/RolesController.java
@@ -36,7 +36,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Controller to handle request for roles data
